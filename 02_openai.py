4af23a820306af40fd0b88cbbded0daa1839930f
Samir Ghimire <samirghimire72456@gmail.com>
2026-02-08 22:29:57 +0545

    Initial commit: AI-powered Chat Bot with secure environment handling

--- 02_openai.py
+++ 02_openai.py
@@ -1,22 +1,15 @@
+import os
 from openai import OpenAI
 
 # 1. Setup the client to point to Google instead of OpenAI
 client = OpenAI(
-    api_key="AIzaSyCDZA5Zm3kxMkssI1t0LTPdCtWS8JOu59s", # Replace with your actual Gemini API key
+    api_key= os.getenv("API_KEY"), # Replace with your actual Gemini API key
     base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
 )
+
 command = '''
-[22:31, 07/02/2026] Niraj Khadka: College janxau hola ni temi
-[22:31, 07/02/2026] Samir Ghimire: Tara space dherai hunxa
-Space pani paisa pani Yetholm rent ka bata tirnu
-[22:32, 07/02/2026] Samir Ghimire: College janxau hola ni temi
-Umm jney ni
-[22:32, 07/02/2026] Niraj Khadka: Space pani paisa pani Yetholm rent ka bata tirnu
-Ahh
-[16:53, 08/02/2026] Samir Ghimire: oee
-[16:54, 08/02/2026] Niraj Khadka: Ha
-[16:54, 08/02/2026] Samir Ghimire: kina naaako clz
-[16:55, 08/02/2026] Niraj Khadka: Nidra pugya thiyena suteko yrr
+[22:31, 07/02/2026] friend: Hi
+[22:31, 07/02/2026] Me : hlo how can i help you
 '''
 
 completion = client.chat.completions.create(
